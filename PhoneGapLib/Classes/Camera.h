80f5b57841caaa23714459592e5b39baa2d93702
ascorbic <ascorbic@gmail.com>
2011-03-21 10:04:00 +0000

    Back out my other changes

--- PhoneGapLib/Classes/Camera.h
+++ PhoneGapLib/Classes/Camera.h
@@ -56,7 +56,6 @@ typedef NSUInteger DestinationType;
 - (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingMediaWithInfo:(NSDictionary*)info;
 - (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingImage:(UIImage*)image editingInfo:(NSDictionary*)editingInfo;
 - (void)imagePickerControllerDidCancel:(UIImagePickerController*)picker;
-- (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController;
 
 - (void) dealloc;
 
